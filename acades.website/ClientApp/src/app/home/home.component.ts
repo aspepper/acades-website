be6f1fe88e7f95538a61e1bcfb783468a08c9af0
alexsandro.pimenta <aspepper@hotmail.com>
2020-10-08 13:01:22 -0300

    Implementação do Carousel na Home

--- acades.website/ClientApp/src/app/home/home.component.ts
+++ acades.website/ClientApp/src/app/home/home.component.ts
@@ -1,9 +1,21 @@
 import { Component } from '@angular/core';
+import * as $ from 'jquery';
+import 'bootstrap';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  ngAfterViewInit() {
+    $('.carousel').carousel();
+  }
+
 }
